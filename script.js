9f0d8383096b91064ae6871095a9c21862266bc9
Elena <42415402+ElenaSamanchuk@users.noreply.github.com>
2025-07-09 14:41:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+document.querySelector(".timer-floating").style.display = "none";
+setTimeout(() => {
+    document.querySelector(".timer-floating").style.display = "flex";
+}, 20000);
 const endDate = new Date("2025-07-10T23:59:59");
 function getWordForm(n, forms) {
   return forms[
